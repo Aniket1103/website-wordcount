2c400d34ac9f91e789333e1d81371fee05e5ce62
Aniket1103 <55594740+Aniket1103@users.noreply.github.com>
2023-07-08 05:16:19 +0530

    refractor - removed unnecessary block of code and added route path

--- server/app.js
+++ server/app.js
@@ -7,32 +7,19 @@ const { countWords } = require('./util/countWords');
 
 app.use(cors());
 
-app.post("/", async (req, res) => {
-    // return res.send("lsfjalj")
+app.post("/insights", async (req, res) => {
     console.log(req.body);
-    
+
     //fetch the HTML content
     const html = await fetchUrlContent("https://www.guthib.com");
-    console.log(html);
 
     //clean the content (convert to text)
     const text = cleanContent(html);
-    // console.log("text-> ", text);
 
     //count the word occurence and send it back
     const words = await countWords(text);
     console.log("Words Count -> ", words);
     res.json({ wordCount : words, favourite : false});
 })
-
-async function fn(){
-    const html = await fetchUrlContent("https://example.com");
-    // console.log(html);
-    const text = cleanContent(html);
-    // console.log("text-> ", text);
-    const words = await countWords(text);
-    console.log("Words Count -> ", words);
-}
-// fn()
 
 module.exports = app;
